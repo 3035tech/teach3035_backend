271830f98535b328d129c31f929fca760a3ad0f3
cleomargigolette <cleomargigolettedasilvanh@gmail.com>
2023-10-05 10:58:12 -0300

    created project variaveis, constantes e tipo

---
+++ variaveis-constantes-tipos/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 23;
+        String nomeCompleto = "Cleomar Gigolette da Silva";
+
+        final String NOME_DO_MEIO = "Gigolette";
+
+    }
+}
