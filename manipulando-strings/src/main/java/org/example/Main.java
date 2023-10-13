c3418ac288bfa0a673b11cea78970f229eafcbfa
cleomargigolette <cleomargigolettedasilvanh@gmail.com>
2023-10-13 13:27:02 -0300

    created project about string

---
+++ manipulando-strings/src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        CONCATENAÇÂO
+//        IMUTABILIDADE
+
+//        String s1 = "1";
+//        String s2 = s1.concat("2");
+//        s2.concat("3");
+//
+//        System.out.println(s2);
+
+//        MÉTODOS IMPORTANTES
+//        equals e equalIgnorecase
+
+//        String s1 = "1";
+//        String s2 = "1";
+//
+//        System.out.println(s1.equals(s2));
+//
+//        String string1 = "cleomar";
+//        String string2 = "Cleomar";
+//
+//        System.out.println(string1.equalsIgnoreCase(string2));
+
+//        start e end
+
+//        String s1 = "cleomar";
+//
+//        System.out.println(s1.startsWith("c"));
+//        System.out.println(s1.endsWith("r"));
+
+//        contain
+//
+//        String s1 = "Cleomar";
+//
+//        System.out.println(s1.contains("mar"));
+
+//        replace
+
+
+//        String s1 = "Cleomar";
+//
+//        System.out.println(s1.replace("C", "c"));
+
+//        trim
+
+//        String s1 = "         Cleomar ";
+//
+//        System.out.println(s1.trim());
+
+//        String s1 = "CLEOMAR";
+//        String s2 = "cleomar";
+//
+//        System.out.println(s2.toUpperCase());
+
+
+//        charAt
+
+        String s1 = "Cleomar";
+
+        System.out.println(s1.charAt(1));
+//        ENCADEAMENTOS
+    }
+}
